79df21b2c7e5c416ea550b9dbe2aeda297f56289
Daniel Basedow <db@connectedcars.dk>
2019-04-15 19:08:17 +0200

    implement authentication check

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use webpki::EndEntityCert;
 use untrusted::Input;
 use serde::Deserialize;
+use base64;
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -8,6 +9,7 @@ pub enum Error {
     Signature,
     ChallengeDoesNotMatch,
     OriginDoesNotMatch,
+    KeyHandleDoesNotMatch,
 }
 
 pub struct RegistrationResponse {
@@ -36,7 +38,7 @@ impl RegistrationResponse {
         }
 
         let cert = EndEntityCert::from(Input::from(&self.attestation_certificate))
-            .map_err(|e| Error::Signature)?;
+            .map_err(|_| Error::Signature)?;
 
         let msg_len = 1 + 32 + 32 + self.key_handle.len() + 65;
         let appid_hash = ring::digest::digest(&ring::digest::SHA256, app_id.as_bytes());
@@ -49,15 +51,14 @@ impl RegistrationResponse {
         msg.extend_from_slice(&self.key_handle);
         msg.extend_from_slice(&self.user_public_key);
 
-        let res = cert.verify_signature(&webpki::ECDSA_P256_SHA256, Input::from(&msg), Input::from(&self.signature))
-            .map_err(|e| Error::Signature)?;
+        cert.verify_signature(&webpki::ECDSA_P256_SHA256, Input::from(&msg), Input::from(&self.signature))
+            .map_err(|_| Error::Signature)?;
         Ok(())
     }
 }
 
 #[test]
 fn test_reg_resp_verify() {
-    use base64;
     let resp = base64::decode_config("BQTq2F9Hc8LbdhiJTczl-yTabd1ZhbOvOvCPd5-mpn05p_Ir1Q6KEbJ_HMbZFr-S4b85k87hMXCI6B0XAONfHk0bQFT93wm0sLD8vF_fVItyB-WJGOETm_I5szGVAPsgtX_sQsRZSdJoGV3D-5ALYBHZwL1G1yVOK4N7i6il8ZlDxJswggJDMIIBLaADAgECAgQX8O1GMAsGCSqGSIb3DQEBCzAuMSwwKgYDVQQDEyNZdWJpY28gVTJGIFJvb3QgQ0EgU2VyaWFsIDQ1NzIwMDYzMTAgFw0xNDA4MDEwMDAwMDBaGA8yMDUwMDkwNDAwMDAwMFowKTEnMCUGA1UEAwweWXViaWNvIFUyRiBFRSBTZXJpYWwgNDAxNjY1MzUwMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEQ-o66R9AJgeKoH6g3FI_WXmvdxUFy__TAreJbnl45E32rKCLQMy2nnKllZs1VfZX136Ff_pQLhUr2BB0C69RpaM7MDkwIgYJKwYBBAGCxAoCBBUxLjMuNi4xLjQuMS40MTQ4Mi4xLjIwEwYLKwYBBAGC5RwCAQEEBAMCBDAwCwYJKoZIhvcNAQELA4IBAQAvwnBqkckkOuQ35S9TJNDHSuAqdwQwRJbeF4KBDEG3ZNHdb1AcS5GL1FfzCGIiCAYVpMvaQZShExivRC204PlK7yj4zLCFds0eF7U6GH9h6JNxZnLXGcXBACk653kzkHBn7LvLIps4U--50K2w0gBQu5HM-B1ev_XXc0MDD4WWwlsY1SdL_w_OFQ-jo5uWCD_surmS-Iqcu5VlZntWzPdIpSeFznGGj7dpGzB676fQsQOizggEB0ikWmur8SqijlrNcMFAlvq0eNAzWNRDCu78b6ad1anwrAEKcanqQDrh4BbEPel9P_Gs6Ft94HYPxkfLFPFeaMJdwASMeXdV8SYVMEUCIQCLyfVmOeJEdK36OYW8JvpdEu7ae9SiaUf5EXnuzWUOBAIgG8M8Rt4B3onL6vdFGwYzzWKgeJGbriyLGxUbVH5KMnc", base64::URL_SAFE_NO_PAD).ok().unwrap();
     let client_data = base64::decode_config("eyJjaGFsbGVuZ2UiOiJkZmdiaG5qa2xtLGZjZ2hqbm0iLCJvcmlnaW4iOiJodHRwczovL2xvY2FsaG9zdDoyMDIwIiwidHlwIjoibmF2aWdhdG9yLmlkLmZpbmlzaEVucm9sbG1lbnQifQ", base64::URL_SAFE_NO_PAD).ok().unwrap();
     let reg_resp = parse_registration_response(&resp).ok().unwrap();
@@ -116,8 +117,6 @@ pub fn parse_registration_response(data: &[u8]) -> Result<RegistrationResponse, 
 
 #[test]
 fn test_parse_registration_response() {
-    use base64;
-
     let reg_resp = "BQR0Dx3Zy4fY74xfS0wZI9FkzFttiNA_FOODMfYCCh2Xpx3126fko3zBGwytGzrDmQn3VzgEG-izO0kmd8kNps1qQIIaAarqUF4CARQYqaTFBII2eqciJpQvqs2hrpEnthHIS_ew-ufy25X4SUQaPRPrgRDmGoSYKHICgVJiOqp1ztMwggE0MIHboAMCAQICCmLqA072q-X-8BMwCgYIKoZIzj0EAwIwFTETMBEGA1UEAxMKVTJGIElzc3VlcjAaFwswMDAxMDEwMDAwWhcLMDAwMTAxMDAwMFowFTETMBEGA1UEAxMKVTJGIERldmljZTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABMwMTq_COwMSIZUJeXsEydPmpFSVMbYaRwcJ3sOM4UMTpg321lv_2SlORsTzFsofAocqp9e0QmMOwBA7xVO4pgWjFzAVMBMGCysGAQQBguUcAgEBBAQDAgQwMAoGCCqGSM49BAMCA0gAMEUCIQDBo6aOLxanIUYnBX9iu3KMngPnobpi0EZSTkVtLC8_cwIgC1945RGqGBKfbyNtkhMifZK05n7fU-gW37Bdnci5D94wRgIhALZYojvL3pUHuCsPggtHU7qXtVPYo0SaE4lSsJ-Zi3VOAiEAvwdx-wjfiqL-ugl5N5LeCxwGtvqrSjmRD1YDt9fe5Gw";
     let bin = base64::decode_config(reg_resp, base64::URL_SAFE_NO_PAD).unwrap();
     let res = parse_registration_response(&bin);
@@ -169,4 +168,76 @@ fn test_parse_der_len() {
 
     let res = parse_der_len(&[0x85, 0x01, 0x34]);
     assert!(res.is_err());
+}
+
+pub struct SignatureData {
+    user_presence: u8,
+    counter: u32,
+    signature: Vec<u8>,
+}
+
+impl SignatureData {
+    pub fn verify(&self, app_id: &str, challenge: &str, client_data: &[u8], public_key: &[u8]) -> Result<(), Error> {
+        let client_data_json = serde_json::from_slice::<ClientData>(client_data).map_err(|_| Error::ParseError)?;
+        if client_data_json.challenge != challenge {
+            return Err(Error::ChallengeDoesNotMatch);
+        }
+
+        let appid_hash = ring::digest::digest(&ring::digest::SHA256, app_id.as_bytes());
+        let client_data_hash = ring::digest::digest(&ring::digest::SHA256, client_data);
+
+        let counter = self.counter.to_be_bytes();
+        let mut msg: Vec<u8> = Vec::with_capacity(69);
+        msg.extend_from_slice(appid_hash.as_ref());
+        msg.push(self.user_presence);
+        msg.extend_from_slice(&counter);
+        msg.extend_from_slice(client_data_hash.as_ref());
+
+        ring::signature::verify(&ring::signature::ECDSA_P256_SHA256_ASN1,
+                                Input::from(public_key),
+                                Input::from(&msg),
+                                Input::from(&self.signature))
+            .map_err(|_| Error::Signature)?;
+
+        Ok(())
+    }
+}
+
+#[test]
+fn test_signature_data_verify() {
+    let pub_key = base64::decode_config("BHQPHdnLh9jvjF9LTBkj0WTMW22I0D8U44Mx9gIKHZenHfXbp-SjfMEbDK0bOsOZCfdXOAQb6LM7SSZ3yQ2mzWo", base64::URL_SAFE_NO_PAD).unwrap();
+    let sig_data = base64::decode_config("AQAAAAcwRgIhAMIZkfT3V843TjB_0Lpe69qSD--mdRVFZTVNa01u01QBAiEAw9sKwHe7lf_hlN63UH7g8k0BUgzsloSkFPOk_34ujEk", base64::URL_SAFE_NO_PAD).unwrap();
+    let sig = parse_signature_data(&sig_data).unwrap();
+    let client_data = base64::decode_config("eyJjaGFsbGVuZ2UiOiJ2Y2V1enZiMzc4Yml1bml1Iiwib3JpZ2luIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6MjAyMCIsInR5cCI6Im5hdmlnYXRvci5pZC5nZXRBc3NlcnRpb24ifQ", base64::URL_SAFE_NO_PAD).unwrap();
+    let res = sig.verify("https://localhost:2020", "vceuzvb378biuniu", &client_data, &pub_key);
+    assert!(res.is_ok());
+}
+
+pub fn parse_signature_data(data: &[u8]) -> Result<SignatureData, Error> {
+    if data.len() < 6 {
+        return Err(Error::ParseError);
+    }
+
+    let user_presence = data[0];
+
+    let counter = u32::from_be_bytes([data[1], data[2], data[3], data[4]]);
+
+    let signature = Vec::from(&data[5..]);
+
+    Ok(SignatureData {
+        user_presence,
+        counter,
+        signature,
+    })
+}
+
+#[test]
+fn test_parse_signature_data() {
+    let sig_data = base64::decode_config("AQAAAAcwRgIhAMIZkfT3V843TjB_0Lpe69qSD--mdRVFZTVNa01u01QBAiEAw9sKwHe7lf_hlN63UH7g8k0BUgzsloSkFPOk_34ujEk", base64::URL_SAFE_NO_PAD).unwrap();
+    let sig = parse_signature_data(&sig_data);
+    assert!(sig.is_ok());
+    let sig = sig.unwrap();
+    assert_eq!(1, sig.user_presence);
+    assert_eq!(7, sig.counter);
+    assert_eq!(vec![0x30, 0x46, 0x02, 0x21, 0x00, 0xc2, 0x19, 0x91, 0xf4, 0xf7, 0x57, 0xce, 0x37, 0x4e, 0x30, 0x7f, 0xd0, 0xba, 0x5e, 0xeb, 0xda, 0x92, 0x0f, 0xef, 0xa6, 0x75, 0x15, 0x45, 0x65, 0x35, 0x4d, 0x6b, 0x4d, 0x6e, 0xd3, 0x54, 0x01, 0x02, 0x21, 0x00, 0xc3, 0xdb, 0x0a, 0xc0, 0x77, 0xbb, 0x95, 0xff, 0xe1, 0x94, 0xde, 0xb7, 0x50, 0x7e, 0xe0, 0xf2, 0x4d, 0x01, 0x52, 0x0c, 0xec, 0x96, 0x84, 0xa4, 0x14, 0xf3, 0xa4, 0xff, 0x7e, 0x2e, 0x8c, 0x49], sig.signature);
 }
